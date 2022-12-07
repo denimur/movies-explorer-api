76f064f11e899bb72a572f4341f2c911b4e762e9
Denis Muradov <denis.muradov@yandex.ru>
2022-12-07 03:02:54 +0300

    исправить опечатку

--- controllers/notFoundController.js
+++ controllers/notFoundController.js
@@ -1,5 +1,5 @@
 const NotFoundError = require('../utils/NotFoundError');
-const { sourceNotFound } = require('../utils/constants').default;
+const { sourceNotFound } = require('../utils/constants');
 
 module.exports.notFoundController = (req, res, next) => {
   next(new NotFoundError(sourceNotFound));
